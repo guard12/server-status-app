b3d6cca1be70d6174a05243449dda4818bc1a750
Matus Kollarcik <matus.kollarcik@gmail.com>
2021-03-29 19:03:40 +0200

    Remove some unused code

--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -2,16 +2,10 @@ import React from 'react'
 
 import { DashboardWrappper, PageHeader } from './styles'
 import { ServersOverview } from './servers-overview'
-import { getServerInfo } from '../../api/get-server-data'
 
 export function Dashboard() {
-    const data = getServerInfo()
-
-    console.log(data)
 	return <DashboardWrappper>
         <PageHeader>Dashboard</PageHeader>
         <ServersOverview />
-
-        <PageHeader>Custom Servers</PageHeader>
     </DashboardWrappper>
 }
